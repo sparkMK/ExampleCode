53cae3d9dd771fbbd72537bcb5a5eeb254e25edb
xingchen <mk_yyy@126.com>
2018-07-14 18:05:40 +0800

    第二次

--- app/src/main/java/com/xingchen/core/PickerView/PickerViewActivity.java
+++ app/src/main/java/com/xingchen/core/PickerView/PickerViewActivity.java
@@ -1,8 +1,20 @@
 package com.xingchen.core.PickerView;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.xingchen.core.R;
+import com.xingchen.core.app.BaseActivity;
+
 /**
  * Created by xingchen on 2018/7/14.
  */
 
-public class PickerViewActivity {
+public class PickerViewActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picker_view);
+    }
 }
