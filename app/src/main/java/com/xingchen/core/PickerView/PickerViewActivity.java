56373e0255df22a6e3d107130c915926630effe4
xingchen <mk_yyy@126.com>
2018-07-14 18:02:21 +0800

    第二次

---
+++ app/src/main/java/com/xingchen/core/PickerView/PickerViewActivity.java
@@ -0,0 +1,8 @@
+package com.xingchen.core.PickerView;
+
+/**
+ * Created by xingchen on 2018/7/14.
+ */
+
+public class PickerViewActivity {
+}
---
+++ app/src/main/java/com/xingchen/core/app/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.xingchen.core.app;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xingchen on 2018/7/14.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
