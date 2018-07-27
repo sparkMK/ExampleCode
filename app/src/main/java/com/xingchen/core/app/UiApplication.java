88c93be4aec3cf986eb98c76d0ae393266e08984
zjn <zhangjunneng>
2018-07-27 09:04:25 +0800

    第二次提交

---
+++ app/src/main/java/com/xingchen/core/app/UiApplication.java
@@ -0,0 +1,15 @@
+package com.xingchen.core.app;
+
+import android.app.Application;
+
+/**
+ * Created by zhangjn on 2018/7/26.
+ */
+
+public class UiApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
