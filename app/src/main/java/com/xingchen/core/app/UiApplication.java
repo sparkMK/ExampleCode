5508c0362f3285e71c8ff5c580bc149cbabc65ab
zjn <zhangjunneng>
2018-08-23 11:27:49 +0800

    第二次提交

--- app/src/main/java/com/xingchen/core/app/UiApplication.java
+++ app/src/main/java/com/xingchen/core/app/UiApplication.java
@@ -3,24 +3,14 @@ package com.xingchen.core.app;
 import android.app.Application;
 
 /**
-<<<<<<< HEAD
  * Created by zhangjn on 2018/7/26.
  */
 
 public class UiApplication extends Application {
-=======
- * Created by xingchen on 2018/7/31.
- */
-
-public class UiApplication extends Application{
->>>>>>> origin/master
 
     @Override
     public void onCreate() {
         super.onCreate();
-<<<<<<< HEAD
-=======
         UEHandler.getInstance().initUEHandler(this);
->>>>>>> origin/master
     }
 }
---
+++ app/src/main/java/com/xingchen/core/widget/bottomDialog/WheelViewGestureListener.java
@@ -0,0 +1,22 @@
+package com.xingchen.core.widget.bottomDialog;
+
+/**
+ * Created by xingchen on 2018/8/5.
+ */
+
+import android.view.MotionEvent;
+
+final class WheelViewGestureListener extends android.view.GestureDetector.SimpleOnGestureListener {
+
+    final WheelView wheelView;
+
+    WheelViewGestureListener(WheelView wheelview) {
+        wheelView = wheelview;
+    }
+
+    @Override
+    public final boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        wheelView.scrollBy(velocityY);
+        return true;
+    }
+}
